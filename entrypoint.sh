5ec56305d4bab6fac71b5e915dfb94824eddad6c
agiwalpooja20 <agiwalpooja20@gmail.com>
2020-05-30 02:36:27 +0530

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,14 +22,16 @@ fi
 echo ::add-path::/google-cloud-sdk/bin/gcloud
 echo ::add-path::/google-cloud-sdk/bin/gsutil
 
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/account.json"
+
 gcloud container clusters create "$CLUSTER_NAME" --zone "$ZONE_NAME" --scopes=https://www.googleapis.com/auth/cloud-platform
 
 gcloud container clusters list
 
 gcloud config set account fission-accel@omni-163105.iam.gserviceaccount.com 
 
-export GOOGLE_APPLICATION_CREDENTIALS="/tmp/account.json"
+rm ~/.kube/config
 
-gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
+gcloud container clusters get-credentials "$CLUSTER_NAME"
 
 kubectl get secrets
