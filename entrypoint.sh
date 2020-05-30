f620f1fca4cb3d2f7137bd633c49303a3f476ba7
agiwalpooja20 <agiwalpooja20@gmail.com>
2020-05-30 18:13:06 +0530

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,20 +22,10 @@ fi
 echo ::add-path::/google-cloud-sdk/bin/gcloud
 echo ::add-path::/google-cloud-sdk/bin/gsutil
 
-export GOOGLE_APPLICATION_CREDENTIALS="/tmp/account.json"
-
-gcloud container clusters create "$CLUSTER_NAME" --zone "$ZONE_NAME" --scopes=https://www.googleapis.com/auth/cloud-platform,userinfo-email
-
-gcloud container clusters list
-
-gcloud config set account fission-accel@omni-163105.iam.gserviceaccount.com 
-
-gcloud config set project "$PROJECT_ID"
-
-gcloud config set compute/zone "$ZONE_NAME"
+#Create cluster
+gcloud container clusters create "$CLUSTER_NAME" --zone "$ZONE_NAME" 
 
 sleep 300
 
+#Update Kubeconfig
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
-
-gcloud config list
