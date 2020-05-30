56e65475d6353b41f92d27d03febb34335274562
agiwalpooja20 <agiwalpooja20@gmail.com>
2020-05-30 17:45:19 +0530

    Changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,6 +34,8 @@ gcloud config set project "$PROJECT_ID"
 
 gcloud config set compute/zone "$ZONE_NAME"
 
+sleep 300
+
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$ZONE_NAME" --project "$PROJECT_ID"
 
 gcloud config list
